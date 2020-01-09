b469bcd20abe02884f16b84467e5b5c7ec5b9fc6
cddbysj <cddbysj@qq.com>
2020-01-09 15:43:58 +0800

    Updates

---
+++ precache-manifest.53b5118520cf6ae043f95dacc0e47a67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e924fb12096e3b5f8c81f69eca4b531",
+    "url": "/my-console/index.html"
+  },
+  {
+    "revision": "794e7edb920027438866",
+    "url": "/my-console/static/css/2.f31ccb54.chunk.css"
+  },
+  {
+    "revision": "49f8ca0494a48c980aed",
+    "url": "/my-console/static/css/main.f57efdf0.chunk.css"
+  },
+  {
+    "revision": "794e7edb920027438866",
+    "url": "/my-console/static/js/2.14564f01.chunk.js"
+  },
+  {
+    "revision": "7c7c92b594adfd350459f1d50416168f",
+    "url": "/my-console/static/js/2.14564f01.chunk.js.LICENSE"
+  },
+  {
+    "revision": "49f8ca0494a48c980aed",
+    "url": "/my-console/static/js/main.6ba57626.chunk.js"
+  },
+  {
+    "revision": "5d946e5c03dbae647e65",
+    "url": "/my-console/static/js/runtime-main.1894d28d.js"
+  }
+]);
